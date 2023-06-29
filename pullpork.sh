a68df22e8e1c85f649390a0e66783c1b4847fae7
ari januari <greez55@gmail.com>
2023-06-30 01:10:07 +0700

    add full cmd

--- pullpork.sh
+++ pullpork.sh
@@ -20,3 +20,53 @@ sudo sed -i 's/^snort_blocklist.*/snort_blocklist = true/' /usr/local/etc/pulled
 sudo sed -i 's/^et_blocklist.*/et_blocklist = true/' /usr/local/etc/pulledpork3/pulledpork.conf
 sudo sed -i 's/^snort_path.*/snort_path = /usr/local/bin/snort/' /usr/local/etc/pulledpork3/pulledpork.conf
 sudo sed -i 's/^local_rules.*/local_rules =  /usr/local/etc/rules/local.rules/' /usr/local/etc/pulledpork3/pulledpork.conf
+
+# Path to the configuration file
+config_file=" /usr/local/etc/snort/snort.lua"
+
+# Lines to be added
+line1="enable_builtin_rules = true,"
+line2="include = RULE_PATH .. \"/pulledpork.rules\","
+
+# Add lines to the configuration file
+sed -i "173i$line1" "$config_file"
+sed -i "174i$line2" "$config_file"
+
+#add So_rules
+snort -c /usr/local/etc/snort/snort.lua --plugin-path /usr/local/etc/so_rules/
+
+#create systemd pulledpork
+cat > /lib/systemd/system/pulledpork3.service << EOL
+[Unit]
+Description=Runs PulledPork3 to update Snort 3 Rulesets
+Wants=pulledpork3.timer
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/pulledpork3/pulledpork.py -c /usr/local/etc/pulledpork3/pulledpork.conf
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo systemctl enable pulledpork3
+sudo service pulledpork3 start
+
+#create systemd timer to run it daily.
+cat > /lib/systemd/system/pulledpork3.timer << EOL
+[Unit]
+Description=Run PulledPork3 rule updater for Snort 3 rulesets
+RefuseManualStart=no # Allow manual starts
+RefuseManualStop=no # Allow manual stops
+[Timer]
+#Execute job if it missed a run due to machine being off
+Persistent=true
+#Run 120 seconds after boot for the first time
+OnBootSec=120
+#Run daily at 1 am
+OnCalendar=*-*-*13:35:00
+#File describing job to execute
+Unit=pulledpork3.service
+[Install]
+WantedBy=timers.target
+EOL
+
+sudo systemctl enable pulledpork3.timer
