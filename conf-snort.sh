f6495b38963bf952c628d1229b1cdbc95b843377
ari januari <greez55@gmail.com>
2023-06-30 01:36:40 +0700

    add cmd conf-snort.sh

--- conf-snort.sh
+++ conf-snort.sh
@@ -4,3 +4,22 @@ sudo mkdir /usr/local/etc/lists/
 sudo touch /usr/local/etc/rules/local.rules
 sudo touch /usr/local/etc/lists/default.blocklist
 sudo mkdir /var/log/snort
+
+cat > /usr/local/etc/rules/local.rules << EOL
+alert icmp any any -> any any ( msg:"ICMP Traffic Detected"; sid:10000001; metadata:policy security-ips alert; )
+EOL
+
+snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/rules/local.rules
+
+# Path to the configuration file
+config_file=" /usr/local/etc/snort/snort.lua"
+
+# Lines to be added
+line1="enable_builtin_rules = true,"
+line2="include = RULE_PATH .. \"/"/local.rules"\","
+
+# Add lines to the configuration file
+sed -i "173i$line1" "$config_file"
+sed -i "174i$line2" "$config_file"
+
+snort -c /usr/local/etc/snort/snort.lua
